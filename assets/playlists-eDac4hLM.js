96e41c99440beb16eccd349db151691cd59cd220
Aphene <aphene.falcon@gmail.com>
2025-07-29 16:25:25 +0300

    Updates

--- assets/playlists-eDac4hLM.js
+++ assets/playlists-eDac4hLM.js
@@ -1,4 +1,4 @@
-import{a as i,c,d,g as m}from"./main-Dz6aGX0i.js";const a=document.getElementById("playlistsContainer"),n=document.getElementById("newPlaylistBtn");async function y(){const t=i.currentUser;if(!t)return;const l=c(d,"users",t.uid,"playlists");(await m(l)).forEach(s=>{const o=s.data(),e=document.createElement("div");e.className="playlist",e.innerHTML=`
+import{a as i,c,d,g as m}from"./main-CHgp6fXb.js";const a=document.getElementById("playlistsContainer"),n=document.getElementById("newPlaylistBtn");async function y(){const t=i.currentUser;if(!t)return;const l=c(d,"users",t.uid,"playlists");(await m(l)).forEach(s=>{const o=s.data(),e=document.createElement("div");e.className="playlist",e.innerHTML=`
       <div></div>
       <p>${o.name}</p>
       <span class="more-icon"><img src="/Binks-Melody/Media/More-icon.svg"></span>
