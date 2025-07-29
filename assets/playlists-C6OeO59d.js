8909e3b2ba83efb84b7ddce3a99f0d44142e9766
Aphene <aphene.falcon@gmail.com>
2025-07-29 21:12:04 +0300

    Updates

---
+++ assets/playlists-C6OeO59d.js
@@ -0,0 +1,5 @@
+import{a as i,g as d,b as m,h as u}from"./main-DEq4oG-G.js";const a=document.getElementById("playlistsContainer"),n=document.getElementById("newPlaylistBtn");async function y(){const t=i.currentUser;if(!t)return;const o=d(m,"users",t.uid,"playlists");(await u(o)).forEach(s=>{const r=s.data(),e=document.createElement("div");e.className="playlist",e.innerHTML=`
+      <div></div>
+      <p>${r.name}</p>
+      <span class="more-icon"><img src="/Binks-Melody/Media/More-icon.svg"></span>
+      `;const l=document.createElement("hr");a.insertBefore(e,n),a.insertBefore(l,n),e.addEventListener("click",c=>{c.target.closest(".more-icon")||(localStorage.setItem("currentPlaylistId",s.id),window.location.href="playlist.html")})})}i.onAuthStateChanged(t=>{t&&y()});n.addEventListener("click",()=>{});
