3efbd53a10f727d834e5c9c4f50418b723a202a2
Aphene <karenmorcos@simplerapps.com>
2025-08-17 21:16:27 +0300

    Updates

---
+++ assets/home-C692aPqd.js
@@ -0,0 +1,14 @@
+import"./firebase-BcYGz8gK.js";import"./main-DNTuINcL.js";const m="804bfeee";async function p(n){const t=`https://api.jamendo.com/v3.0/tracks/?client_id=${m}&format=json&limit=10&search=${encodeURIComponent(n)}`;return(await(await fetch(t)).json()).results}const y=document.querySelector(".search-input"),r=document.querySelector(".section-title"),d=document.querySelector(".grid-container"),o=document.getElementById("search-results");document.querySelector(".player");const e=document.getElementById("player-audio"),g=document.getElementById("p-title"),E=document.getElementById("p-artist"),v=document.getElementById("p-cover"),f=document.getElementById("p-current");document.getElementById("p-duration");const i=document.getElementById("p-seek"),u=document.getElementById("p-play");document.getElementById("p-prev");document.getElementById("p-next");function h(n){o.innerHTML="",n.forEach(t=>{const s=document.createElement("div"),a=document.createElement("div");a.classList.add("song-item"),a.innerHTML=`
+        <div class="song-left">
+            <div class="song-thumb">
+            <img src="${t.album_image}" alt="Album Art">
+            </div>
+            <div class="song-info">
+            <span class="song-title">${t.name}</span>
+            <span class="song-artist">${t.artist_name}</span>
+            </div>
+        </div>
+        <button class="song-menu">
+            <img class="more-icon" src="/Binks-Melody/Media/More-icon.svg">
+        </button>
+        `,a.querySelector(".song-menu").addEventListener("click",()=>I(t.audio,t)),s.appendChild(a);const l=document.createElement("hr");l.classList.add("song-separator"),s.appendChild(l),o.appendChild(s)})}function I(n,t){g.textContent=t.name,E.textContent=t.artist_name,v.src=t.album_image,document.getElementById("player").classList.add("open"),e.pause(),e.src=n,e.load(),e.addEventListener("canplay",function s(){e.removeEventListener("canplay",s),e.play().catch(a=>{console.warn("Playback failed:",a)})})}e.addEventListener("timeupdate",()=>{if(e.duration){const n=e.currentTime/e.duration*100;i.value=n,f.textContent=B(e.currentTime)}});i.addEventListener("input",()=>{e.duration&&(e.currentTime=i.value/100*e.duration)});const c=document.getElementById("play-icon");u&&u.addEventListener("click",()=>{e.paused?(e.play(),c.src="public/Media/Pause-icon.svg",c.alt="Pause"):(e.pause(),c.src="public/Media/Play-icon.svg",c.alt="Play")});function B(n){const t=Math.floor((n||0)/60),s=Math.floor((n||0)%60);return`${t}:${s<10?"0":""}${s}`}y.addEventListener("input",async n=>{const t=n.target.value.trim();if(t.length>2){const s=await p(t);h(s),r.style.display="none",d.style.display="none",o.style.display="flex"}else o.innerHTML="",r.style.display="flex",d.style.display="flex",o.style.display="none"});
