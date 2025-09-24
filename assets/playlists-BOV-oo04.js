0f642a742b1af2f2c7eea80fbd3159fb9a22d32b
Aphene <aphene.falcon@gmail.com>
2025-09-24 20:57:08 +0200

    Updates

--- assets/playlists-BOV-oo04.js
+++ assets/playlists-BOV-oo04.js
@@ -1,4 +1,4 @@
-import{b as c,h,i as p,j as B,e as f,u as E,k}from"./firebase-DoM9rtEd.js";import"./main-D917LwOw.js";const u=document.getElementById("playlistsContainer")||null,m=document.getElementById("newPlaylistBtn")||null,t=document.createElement("div");t.className="playlist-popup hidden";t.innerHTML=`
+import{b as c,h,i as p,j as B,e as f,u as E,k}from"./firebase-Bf4RwclJ.js";/* empty css              */import"./main-DRKiw7_W.js";const u=document.getElementById("playlistsContainer")||null,m=document.getElementById("newPlaylistBtn")||null,t=document.createElement("div");t.className="playlist-popup hidden";t.innerHTML=`
   <div class="popup-item" id="renameBtn">
     <img src="/Binks-Melody/Media/Rename-icon.svg" alt="Rename">
     <span>Rename</span>
